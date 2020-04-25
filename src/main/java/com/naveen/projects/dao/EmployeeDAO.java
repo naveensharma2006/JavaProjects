c281952506072895b2fdca869ee46cdacdea9a39
naveensharma2006 <57703369+naveensharma2006@users.noreply.github.com>
2020-04-25 22:43:58 +0530

    Added comments

--- src/main/java/com/naveen/projects/dao/EmployeeDAO.java
+++ src/main/java/com/naveen/projects/dao/EmployeeDAO.java
@@ -4,6 +4,10 @@ package com.naveen.projects.dao;
 
 import com.naveen.projects.pojos.Employee;
 
+/**
+ * @author naveensharma
+ *
+ */
 public interface EmployeeDAO {
 	
 	public int getEmployeeID(String name);
